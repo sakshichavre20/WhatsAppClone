1b7dd8d7076d27527bbcc4eda832c280f8896b7c
Sakshi Chavre <chavresakshi@gmail.com>
2021-07-14 21:27:57 +0530

    Initalized first react project
    cloning whatsapp's frontend part

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+function Chat() {
+    return (
+        <div className="Chat">
+            <h1>Chat</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/Sidebar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Sidebar.css'
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar, IconButton } from '@material-ui/core';
+function Sidebar() {
+    return (
+      <div className="Sidebar">
+        <div className="sidebar__header">
+          <Avatar src="https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+          <div className="sidebar__headerRight">
+            <IconButton>
+              <DonutLargeIcon style={{ color: "#B1B3B5" }} />
+            </IconButton>
+            <IconButton>
+              <ChatIcon style={{ color: "#B1B3B5" }} />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon style={{ color: "#B1B3B5" }} />
+            </IconButton>
+          </div>
+        </div>
+        <div className="sidebar__searchbar">
+          <div className="sidebar__searchcontainer">
+            <SearchIcon style={{ color: "#848788" }} />
+            <input
+              type="text"
+              placeholder="search or a new chat"
+              style={{ background: "#323739" }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Sidebar
